a1d872ae36df75dd56b0fd61b53b6a167608b55e
krasavchik1 <v1osnovenko@gmail.com>
2024-02-28 06:21:16 +0200

    commit test_utils and adding step by run auto tests

---
+++ README.md
@@ -0,0 +1,10 @@
+- Download the project from the repository
+
+- To run tests look at package.json -> scripts. 
+  Enter the following command to terminal:
+  
+  npm run "script which you want to run"
+
+Example:
+        If you want to run login.cy.js, use this command:
+        npm run registration
--- cypress/e2e/test_utils.cy.js
+++ cypress/e2e/test_utils.cy.js
@@ -1,96 +1,99 @@
-import { registrationPage } from "../page_object/add_item";
-const utils = require("../support/utils");
+//for future achievements
 
-const registration_page = new registrationPage();
 
-describe("Registration Test", () => {
-  let emailAddress;
-  let inboxId;
+// import { registrationPage } from "../page_object/add_item";
+// const utils = require("../support/utils");
 
-  before(() => {
-    cy.fixture("user_data").then(function (data) {
-      this.data = data;
-    });
+// const registration_page = new registrationPage();
 
-    return utils.createInbox().then((inbox) => {
-      emailAddress = inbox.emailAddress;
-      inboxId = inbox.id;
-      utils.saveRegistrationEmail(emailAddress);
-      cy.writeFile("email.json", { emailAddress: emailAddress });
-    });
-  });
+// describe("Registration Test", () => {
+//   let emailAddress;
+//   let inboxId;
 
-  it("going to website", () => {
-    cy.visit("https://winzor.dev-devocean.pro/");
-  });
+//   before(() => {
+//     cy.fixture("user_data").then(function (data) {
+//       this.data = data;
+//     });
 
-  it("authorization btn", () => {
-    registration_page.clickSignIn();
-    registration_page.clickRegistration();
-  });
+//     return utils.createInbox().then((inbox) => {
+//       emailAddress = inbox.emailAddress;
+//       inboxId = inbox.id;
+//       utils.saveRegistrationEmail(emailAddress);
+//       cy.writeFile("email.json", { emailAddress: emailAddress });
+//     });
+//   });
 
-  it("fill first name", () => {
-    registration_page.userName();
-  });
+//   it("going to website", () => {
+//     cy.visit("https://winzor.dev-devocean.pro/");
+//   });
 
-  it("fill second name", () => {
-    registration_page.userSecondName();
-  });
+//   it("authorization btn", () => {
+//     registration_page.clickSignIn();
+//     registration_page.clickRegistration();
+//   });
 
-  it("fill password", () => {
-    registration_page.userPassword();
-  });
+//   it("fill first name", () => {
+//     registration_page.userName();
+//   });
 
-  it("fill conf password", () => {
-    registration_page.userConfPassword();
-  });
+//   it("fill second name", () => {
+//     registration_page.userSecondName();
+//   });
 
-  it("fill business email", () => {
-    cy.get("input.v-field__input").eq(2).should("exist").type(emailAddress);
-  });
+//   it("fill password", () => {
+//     registration_page.userPassword();
+//   });
 
-  it("registration btn", () => {
-    registration_page.clickRegistrationBtn();
-  });
-  it("insert the otp", () => {
-    cy.waitForLatestEmail(inboxId, { count: 2 }).then((emails) => {
-      for (let i = 0; i < emails.length; i++) {
-        const emailBody = emails[i].body;
+//   it("fill conf password", () => {
+//     registration_page.userConfPassword();
+//   });
 
-        if (emailBody.includes("Verify Email")) {
-          cy.log(
-            `Найдено письмо с заголовком "Verify Email" в ${i + 1}-м письме.`
-          );
+//   it("fill business email", () => {
+//     cy.get("input.v-field__input").eq(2).should("exist").type(emailAddress);
+//   });
 
-          // Открываем письмо
-          cy.log(`Пытаемся открыть письмо с ID: ${emails[i].id}`);
-          cy.visit(`/inbox/${inboxId}/emails/${emails[i].id}`);
+//   it("registration btn", () => {
+//     registration_page.clickRegistrationBtn();
+//   });
+//   it("insert the otp", () => {
+//     cy.waitForLatestEmail(inboxId, { count: 2 }).then((emails) => {
+//       for (let i = 0; i < emails.length; i++) {
+//         const emailBody = emails[i].body;
 
-          cy.get(".email").then(($email) => {
-            if ($email.length === 0) {
-              cy.log("Письмо не загрузилось");
-              return;
-            }
+//         if (emailBody.includes("Verify Email")) {
+//           cy.log(
+//             `Найдено письмо с заголовком "Verify Email" в ${i + 1}-м письме.`
+//           );
 
-            cy.get("a")
-              .contains("Посилання")
-              .then(($link) => {
-                if ($link.length === 0) {
-                  cy.log("Ссылка в письме не найдена");
-                  return;
-                }
-                const targetLink = $link.attr("href");
-                cy.log("Найдена ссылка в письме:", targetLink);
+//           // Открываем письмо
+//           cy.log(`Пытаемся открыть письмо с ID: ${emails[i].id}`);
+//           cy.visit(`/inbox/${inboxId}/emails/${emails[i].id}`);
 
-                cy.visit(targetLink);
-              });
-          });
-        } else {
-          cy.log(
-            `Письмо с заголовком "Verify Email" не найдено в ${i + 1}-м письме.`
-          );
-        }
-      }
-    });
-  });
-});
+//           cy.get(".email").then(($email) => {
+//             if ($email.length === 0) {
+//               cy.log("Письмо не загрузилось");
+//               return;
+//             }
+
+//             cy.get("a")
+//               .contains("Посилання")
+//               .then(($link) => {
+//                 if ($link.length === 0) {
+//                   cy.log("Ссылка в письме не найдена");
+//                   return;
+//                 }
+//                 const targetLink = $link.attr("href");
+//                 cy.log("Найдена ссылка в письме:", targetLink);
+
+//                 cy.visit(targetLink);
+//               });
+//           });
+//         } else {
+//           cy.log(
+//             `Письмо с заголовком "Verify Email" не найдено в ${i + 1}-м письме.`
+//           );
+//         }
+//       }
+//     });
+//   });
+// });
